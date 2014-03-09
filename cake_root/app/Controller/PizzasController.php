6f21efdc548d5ae7649c0cfafa74a052a73ac4a7
James Pickard <james.pickard+github@gmail.com>
2014-03-09 17:27:24 -0400

    move toppings into a class property

--- cake_root/app/Controller/PizzasController.php
+++ cake_root/app/Controller/PizzasController.php
@@ -1,5 +1,24 @@
 <?php
 class PizzasController extends AppController {
+	private $toppings = array(
+		'bacon',
+		'cajun chicken',
+		'chorizo',
+		'honey cured ham',
+		'pepperoni',
+		'turkey',
+		'cheddar',
+		'feta',
+		'gorgonzola',
+		'monterey jack',
+		'provolone',
+		'roquefort',
+		'pistachios',
+		'pecans',
+		'pine nuts',
+		'walnuts',
+	);
+
 	public $helpers = array(
 		'Html' => array('className' => 'BoostCake.BoostCakeHtml'),
 		'Form',
@@ -10,25 +29,12 @@ class PizzasController extends AppController {
     }
 
     public function makePizza() {
-        $this->set('pizzas', $this->Pizza->find('all'));
-		$this->set('toppings', Array(
-			'bacon',
-			'cajun chicken',
-			'chorizo',
-			'honey cured ham',
-			'pepperoni',
-			'turkey',
-			'cheddar',
-			'feta',
-			'gorgonzola',
-			'monterey jack',
-			'provolone',
-			'roquefort',
-			'pistachios',
-			'pecans',
-			'pine nuts',
-			'walnuts',
-		));
+		// Bind the toppings to diplay the pizza creator.
+		// TODO: Move these toppings to a database table.
+		$this->set('toppings', $this->toppings);
+
+		// request->data not empty, save the pizza.
+		debug($this->request->data);
     }
 }
 ?>
