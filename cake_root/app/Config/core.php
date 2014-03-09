8fc9f741158fcde3f8b1eb188073d8d2db66e801
James Pickard <james.pickard+github@gmail.com>
2014-03-09 11:39:47 -0400

    update salt and cipherseed

--- cake_root/app/Config/core.php
+++ cake_root/app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '2599b8879e0f0ebb1a9eec645d7cf290eec08b0c');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '5cd2178d271da4942873ff514fc07f8635f8a53e');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
