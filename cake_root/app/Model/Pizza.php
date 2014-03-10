c20480e3a272caf6ccbfa1df4a48c281ea631a17
James Pickard <james.pickard+github@gmail.com>
2014-03-10 09:47:22 -0400

    move success message to the model

--- cake_root/app/Model/Pizza.php
+++ cake_root/app/Model/Pizza.php
@@ -1,4 +1,22 @@
 <?php
 class Pizza extends AppModel {
+	public $belongsTo = array(
+        'User' => array(
+            'className' => 'User',
+            'foreignKey' => 'customer_id'
+        )
+    );
+
+	// TODO: Validate toppings.
+
+	public $success_message = '';
+
+	public function afterSave($created, $options = array()) {
+		$num_orders = $this->find('count', array(
+			'conditions' => array('customer_id' => $this->data['Pizza']['customer_id'])
+		));
+
+		$this->success_message = __(sprintf('Pizza #%d ordered.', $num_orders));
+	}
 }
 ?>
