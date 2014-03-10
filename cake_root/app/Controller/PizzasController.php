dde7417e96a5a20d518377a4ef33064849173e76
James Pickard <james.pickard+github@gmail.com>
2014-03-10 09:44:04 -0400

    add page title, simplify makePizza logic

--- cake_root/app/Controller/PizzasController.php
+++ cake_root/app/Controller/PizzasController.php
@@ -1,5 +1,6 @@
 <?php
 class PizzasController extends AppController {
+	// TODO: Move these toppings to the database.
 	private $toppings = array(
 		'bacon',
 		'cajun chicken',
@@ -24,17 +25,54 @@ class PizzasController extends AppController {
 		'Form',
 		'Js');
 
-    public function index() {
-        $this->set('pizzas', $this->Pizza->find('all'));
-    }
+	public function beforeFilter() {
+		parent::beforeFilter();
+
+		// Allow users to make a pizza before logging in.
+		// TODO: Only allow admins to viewOrders.
+		$this->Auth->allow('viewOrders', 'makePizza');
+	}
 
     public function makePizza() {
 		// Bind the toppings to diplay the pizza creator.
-		// TODO: Move these toppings to a database table.
 		$this->set('toppings', $this->toppings);
+		$this->set('title_for_layout', 'Pizza Builder');
 
-		// request->data not empty, save the pizza.
-		debug($this->request->data);
+		// request->data is empty - no more to do.
+		if ($this->request->is('post') === false) {
+			return;
+		}
+
+		// Save the pizza.
+
+		// Check whether the user is logged in.
+		// If the user is logged in, we can order the pizza right away. If
+		// not, we'll save it in their session and prompt them to login.
+		$user = $this->Auth->user();
+
+		if($user) {
+			$this->request->data['Pizza'] = array_merge(
+				$this->request->data['Pizza'],
+				array('customer_id' => $user['id']));
+
+			$this->Pizza->save($this->request->data);
+			$this->Session->setFlash($this->Pizza->success_message);
+		} else {
+			$this->Session->write('order', $this->request->data['Pizza']);
+
+			$this->Session->setFlash(__(
+				'Order saved - login or register to complete order.'),
+			'success');
+
+			return $this->redirect($this->Auth->loginAction);
+		}
     }
+
+	public function viewOrders() {
+		$this->set('pizzas', $this->Pizza->find('all', array(
+			'contain' => array('User'),
+			'order' => array('Pizza.id'),
+		)));
+	}
 }
 ?>
