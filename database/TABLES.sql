f6a1e9df179e4cf137d8f54c57a9e49b2c22f52a
James Pickard <james.pickard+github@gmail.com>
2014-03-09 20:09:25 -0400

    customer_id must not be null

--- database/TABLES.sql
+++ database/TABLES.sql
@@ -11,7 +11,7 @@ CREATE TABLE customers (
 
 CREATE TABLE pizzas (
 	id             INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	customer_id    INT UNSIGNED,
+	customer_id    INT UNSIGNED NOT NULL,
 	topping1       VARCHAR(255),
 	topping2       VARCHAR(255),
 	topping3       VARCHAR(255),
