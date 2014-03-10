436dd5ae1d3614ab0fbd2a1c5b47d57b40f4fbec
James Pickard <james.pickard+github@gmail.com>
2014-03-10 09:46:12 -0400

    add username and password to the model

--- cake_root/app/Model/User.php
+++ cake_root/app/Model/User.php
@@ -14,5 +14,21 @@ class User extends AppModel {
 		}
 		return true;
 	}
+
+	// TODO: Validate username more strictly: length, non-word characters.
+	public $validate = array(
+        'username' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A username is required'
+            )
+        ),
+        'password' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A password is required'
+            )
+        ),
+    );
 }
 ?>
